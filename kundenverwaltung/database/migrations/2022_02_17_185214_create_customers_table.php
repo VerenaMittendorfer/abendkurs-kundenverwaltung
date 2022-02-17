c5c50cbbdda8008fc3dd0220de5e649725eabd65
VerenaMittendorfer <verena.mittendorfer@gmail.com>
2022-02-17 20:29:44 +0100

    migrationen erstellt

---
+++ kundenverwaltung/database/migrations/2022_02_17_185214_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('forename', 150);
+            $table->string('lastname', 150);
+            $table->string('email', 150);
+            $table->string('function', 150)->nullable();
+            $table->text('note')->nullable();
+            $table->foreignId('company_id')->constrained('companies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
